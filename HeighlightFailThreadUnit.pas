0407e92cf992babdf1ad8cd2d605baad52c90bc7
AnnihilatoR <AnnihilatoR@localhost>
2025-11-20 14:30:11 +0300

    upd

---
+++ HeighlightFailThreadUnit.pas
@@ -0,0 +1,140 @@
+﻿unit HeighlightFailThreadUnit;
+
+interface
+
+uses
+    System.Classes
+  , System.SyncObjs
+  , FMX.Controls
+  , ThreadFactoryUnit
+  ;
+
+type
+  TAfterDestroyProcRef = reference to procedure;
+
+  THeighlightFailThread = class(TThreadExt)
+  strict private
+    FCriticalSection: TCriticalSection;
+    FControl: TControl;
+    FOnAfterDestroyProcRef: TAfterDestroyProcRef;
+
+    procedure SetAfterDestroyProcRef(
+      const AAfterDestroyProcRef: TAfterDestroyProcRef);
+  protected
+    procedure Execute(const AThread: TThreadExt); reintroduce; // override;
+  public
+    constructor Create(
+      const AThreadFactory: TThreadFactory;
+      const AThreadName: String;
+      const AControl: TControl); reintroduce;
+
+    destructor Destroy; override;
+
+    property OnAfterDestroyProcRef: TAfterDestroyProcRef
+      write SetAfterDestroyProcRef;
+  end;
+
+implementation
+
+uses
+    System.SysUtils
+  , System.Generics.Collections
+  , FMX.StdCtrls
+  , ToolsUnit
+  , ConstantsUnit
+  ;
+
+{ THeighlightFailThread }
+
+constructor THeighlightFailThread.Create(
+  const AThreadFactory: TThreadFactory;
+  const AThreadName: String;
+  const AControl: TControl);
+begin
+  FCriticalSection := TCriticalSection.Create;
+  FControl := AControl;
+  FOnAfterDestroyProcRef := nil;
+
+  FreeOnTerminate := true;
+
+  inherited Create(
+    AThreadFactory,
+    AThreadName,
+    Execute,
+    false);
+end;
+
+destructor THeighlightFailThread.Destroy;
+var
+  _OnAfterDestroyProcRef: TAfterDestroyProcRef;
+begin
+  FreeAndNil(FCriticalSection);
+
+  inherited;
+
+  if Assigned(FOnAfterDestroyProcRef) then
+  begin
+    _OnAfterDestroyProcRef := FOnAfterDestroyProcRef;
+    TThread.ForceQueue(nil,
+      procedure
+      begin
+        _OnAfterDestroyProcRef;
+      end);
+  end;
+end;
+
+procedure THeighlightFailThread.SetAfterDestroyProcRef(
+  const AAfterDestroyProcRef: TAfterDestroyProcRef);
+begin
+  FCriticalSection.Enter;
+  try
+    FOnAfterDestroyProcRef := AAfterDestroyProcRef;
+  finally
+    FCriticalSection.Leave;
+  end;
+end;
+
+procedure THeighlightFailThread.Execute(const AThread: TThreadExt);
+var
+  Control: TControl;
+  Count: Integer;
+begin
+  Control := FControl;
+  Count := 20;
+
+  Queue(nil,
+    procedure
+    begin
+      TTools.GlowEffectActivated(
+        FAIL_HEIGHLIGTH_GLOW_EFFECT_IDENT,
+        Control,
+        true);
+    end
+  );
+
+  while (Count > 0) and not Terminated do
+  begin
+    Dec(Count);
+
+    Sleep(100);
+  end;
+
+  Queue(nil,
+    procedure
+    begin
+      TTools.GlowEffectActivated(
+        FAIL_HEIGHLIGTH_GLOW_EFFECT_IDENT,
+        Control,
+        false);
+    end
+  );
+
+  if not Terminated then
+  begin
+    HoldThread;
+    ExecHold;
+  end;
+end;
+
+
+end.
